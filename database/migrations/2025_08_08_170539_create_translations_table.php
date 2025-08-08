55464ea6848cc95b1e3da8cdb37483796e682d95
maikeru-desu <gelvezmichael@yahoo.com>
2025-08-09 03:36:47 +0800

    feat: Add migrations for translatinons

---
+++ database/migrations/2025_08_08_170539_create_translations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('locale_id')->constrained('locales')->onDelete('cascade');
+            $table->string('key', 255);
+            $table->text('content');
+            $table->timestamps();
+
+            $table->index('key');
+
+            $table->unique(['locale_id', 'key']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('translations');
+    }
+};
